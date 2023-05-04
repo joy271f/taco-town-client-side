f99586e704d0c3ea6d887c7c8c8f31da25dc7752
joychandradas <joychandradas007@gmail.com>
2023-05-04 12:18:54 +0600

    Basic SetUp Done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from 'react-bootstrap'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <Button variant="success">Success</Button>
+    </>
+  )
+}
+
+export default App
