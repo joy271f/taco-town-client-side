89e152099ad2056894121457b29120b9b71c8648
joychandradas <joychandradas007@gmail.com>
2023-05-05 19:33:40 +0600

    chef detailes data load done

--- src/pages/Home/ChefDetails/ChefDetails.jsx
+++ src/pages/Home/ChefDetails/ChefDetails.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const ChefDetails = () => {
-    
+    const chefDetailsData = useLoaderData();
+    const [chefs, setChefs] = useState([]);
+
+    const { recipe_name, image, ingredients, cooking_method, rating } = chefDetailsData[0]
+    const { name, year_of_experience, number_of_recipes, likes, img, short_bio } = chefs[0]
+    useEffect(() => {
+        fetch('http://localhost:5000/chef')
+            .then(res => res.json())
+            .then(data => setChefs(data))
+    }, [])
     return (
         <div>
-            <h1>chef Details</h1>
+            <h1>{recipe_name}</h1>
         </div>
     );
 };
