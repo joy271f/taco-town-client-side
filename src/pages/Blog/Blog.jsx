f3323f1e5c06a0be5fc280eb195b5cae5de85eac
joychandradas <joychandradas007@gmail.com>
2023-05-08 13:51:30 +0600

    Added tooltip

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -21,7 +21,7 @@ const Blog = () => {
                             <Card.Body>
                                 <Card.Title>What is the differences between uncontrolled and controlled components ?</Card.Title>
                                 <Card.Text>
-                                In React, controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally.
+                                    In React, controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
@@ -32,7 +32,7 @@ const Blog = () => {
                             <Card.Body>
                                 <Card.Title>How to validate React props using PropTypes</Card.Title>
                                 <Card.Text>
-                                Usually, you need to define some custom validation logic for component props, for example, to ensure that a prop is passed a valid email address. prop-types allows you to define custom validation functions that you can use for type checking props.
+                                    Usually, you need to define some custom validation logic for component props, for example, to ensure that a prop is passed a valid email address. prop-types allows you to define custom validation functions that you can use for type checking props.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
@@ -43,7 +43,7 @@ const Blog = () => {
                             <Card.Body>
                                 <Card.Title>what is difference between nodejs and express js ?</Card.Title>
                                 <Card.Text>
-                                Express is a minimal and flexible node. js web application framework, providing a robust set of features for building single and multi-page, and hybrid web applications. On the other hand, Node. js is detailed as "A platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications".
+                                    Express is a minimal and flexible node. js web application framework, providing a robust set of features for building single and multi-page, and hybrid web applications. On the other hand, Node. js is detailed as "A platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications".
                                 </Card.Text>
                             </Card.Body>
                         </Card>
@@ -54,7 +54,7 @@ const Blog = () => {
                             <Card.Body>
                                 <Card.Title>What is a custom hook, and why will you create a custom hook?</Card.Title>
                                 <Card.Text>
-                                Custom React JS hooks offer reusability as when a custom hook is created, it can be reused easily, which makes the code cleaner and reduces the time to write the code. It also enhances the rendering speed of the code as a custom hook does not need to be rendered again and again while rendering the whole code
+                                    Custom React JS hooks offer reusability as when a custom hook is created, it can be reused easily, which makes the code cleaner and reduces the time to write the code. It also enhances the rendering speed of the code as a custom hook does not need to be rendered again and again while rendering the whole code
                                 </Card.Text>
                             </Card.Body>
                         </Card>
